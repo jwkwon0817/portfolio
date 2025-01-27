09f7e088f711b5ffa260c1f1663b225a9605b525
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-01-28 08:30:25 +0900

    feat: enhance DefaultButton with className prop for custom styling

--- src/components/Button/categories/default.tsx
+++ src/components/Button/categories/default.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import cn from 'classnames';
 import Icon from '~/components/Icon';
 import { IconName } from '~/components/Icon/icon-set';
 import HStack from '~/components/Layout/HStack';
@@ -5,19 +8,20 @@ import { useBreakpoint } from '~/hooks/useBreakpoint';
 import * as s from '../styles/default.css';
 
 interface DefaultButtonProps {
+  className?: string;
   children: string;
   onClick?: () => void;
 }
 
 export default function DefaultButton(props: DefaultButtonProps) {
-  const { onClick, children } = props;
+  const { onClick, children, className } = props;
 
   const { isMobile } = useBreakpoint();
 
   return (
     <HStack
       onClick={onClick}
-      classNames={s.base}
+      classNames={cn(s.base, className)}
       padding={isMobile ? 8 : 12}
       spacing={8}>
       <Icon name={IconName.ARROW_DOWNWARD} size={isMobile ? 16 : 20} />

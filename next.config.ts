12c291d6a5125403d1bc62d2edea154ae882b4b5
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-01-28 08:20:35 +0900

    🎉 `jwkwon0817/portfolio` initialized!

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+import type { NextConfig } from 'next';
+
+const withVanillaExtract = createVanillaExtractPlugin({
+  identifiers: 'short',
+});
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
+
+export default withVanillaExtract(nextConfig);

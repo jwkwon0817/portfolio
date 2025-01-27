87c4ac0dfe839781b08a6990757035281656a250
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-01-28 08:53:55 +0900

    feat: add GitHub and Mail SVG icons to Icon component

--- src/components/Icon/icon-set.ts
+++ src/components/Icon/icon-set.ts
@@ -1,9 +1,15 @@
 import ArrowDownward from './assets/arrow_downward.svg';
+import GitHub from './assets/github.svg';
+import Mail from './assets/mail.svg';
 
 export enum IconName {
   ARROW_DOWNWARD = 'arrow_downward',
+  MAIL = 'mail',
+  GITHUB = 'github',
 }
 
 export const IconNameMap = {
   [IconName.ARROW_DOWNWARD]: ArrowDownward,
+  [IconName.MAIL]: Mail,
+  [IconName.GITHUB]: GitHub,
 } as const;

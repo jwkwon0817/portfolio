b028195fa226481b6f514e11a940e90772e2c471
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-01-29 03:05:08 +0900

    feat: add path alias '@' for src directory in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const withVanillaExtract = createVanillaExtractPlugin({
   identifiers: 'short',
@@ -12,6 +13,11 @@ const nextConfig: NextConfig = {
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
 
     return config;
   },

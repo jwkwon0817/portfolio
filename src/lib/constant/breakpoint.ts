e40ac02ff2ce72b98d7bcb663215cececebaae70
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-01-28 10:52:30 +0900

    feat: update breakpoints for responsive design with new mobile and desktop ranges

--- src/lib/constant/breakpoint.ts
+++ src/lib/constant/breakpoint.ts
@@ -1,4 +1,5 @@
 export const breakpoints = {
-  mobile: 'screen and (max-width: 767px)',
-  desktop: 'screen and (min-width: 768px)',
+  mobile: 'screen and (max-width: 899px)',
+  mobileDesktop: 'screen and (min-width: 900px) and (max-width: 1499px)',
+  desktop: 'screen and (min-width: 1500px)',
 } as const;

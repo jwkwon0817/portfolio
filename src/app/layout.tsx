e41ad7336e8aca597adada225ec5baa624bd26df
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-01-29 03:39:19 +0900

    feat: enhance metadata with Open Graph properties for improved SEO and sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,20 @@ import '@/styles/global.css';
 export const metadata: Metadata = {
   title: 'Jeewon Kwon',
   description: 'Sunrin Internet High School Student',
+  openGraph: {
+    title: 'Jeewon Kwon',
+    description: 'Sunrin Internet High School Student',
+    type: 'website',
+    locale: 'ko_KR',
+    url: 'https://www.jwkwon0817.me',
+    siteName: 'Jeewon Kwon',
+    images: [
+      {
+        url: 'https://www.jwkwon0817.me/_next/image?url=%2Fassets%2Fprofile.png&w=256&q=75', // OG 이미지 경로
+        alt: 'Jeewon Kwon',
+      },
+    ],
+  },
 };
 
 export const viewport: Viewport = {

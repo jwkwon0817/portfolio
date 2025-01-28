cb28f5d2e7c767e58330730fc783ac0de2c816eb
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-01-28 09:29:09 +0900

    feat: add new SVG icons for Copy, Instagram, Phone, and Velog to Icon component

--- src/components/Icon/icon-set.ts
+++ src/components/Icon/icon-set.ts
@@ -1,15 +1,27 @@
 import ArrowDownward from './assets/arrow_downward.svg';
+import Copy from './assets/copy.svg';
 import GitHub from './assets/github.svg';
+import Instagram from './assets/instagram.svg';
 import Mail from './assets/mail.svg';
+import Phone from './assets/phone.svg';
+import Velog from './assets/velog.svg';
 
 export enum IconName {
   ARROW_DOWNWARD = 'arrow_downward',
   MAIL = 'mail',
   GITHUB = 'github',
+  VELOG = 'velog',
+  PHONE = 'phone',
+  INSTAGRAM = 'instagram',
+  COPY = 'copy',
 }
 
 export const IconNameMap = {
   [IconName.ARROW_DOWNWARD]: ArrowDownward,
   [IconName.MAIL]: Mail,
   [IconName.GITHUB]: GitHub,
+  [IconName.VELOG]: Velog,
+  [IconName.PHONE]: Phone,
+  [IconName.INSTAGRAM]: Instagram,
+  [IconName.COPY]: Copy,
 } as const;

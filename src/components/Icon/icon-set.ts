b230cf39d7d88015c210e3272efe04af1e212c07
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-01-28 13:23:59 +0900

    feat: add new SVG icons for school, kotlin, pencil, motion, swiftui, nextjs, trophy, spring-boot, storybook, and typescript

--- src/components/Icon/icon-set.ts
+++ src/components/Icon/icon-set.ts
@@ -1,9 +1,29 @@
 import ArrowDownward from './assets/arrow_downward.svg';
+import Bun from './assets/bun.svg';
 import Copy from './assets/copy.svg';
+import Docker from './assets/docker.svg';
+import Embedded from './assets/embedded.svg';
+import FastAPI from './assets/fastapi.svg';
 import GitHub from './assets/github.svg';
 import Instagram from './assets/instagram.svg';
+import Kotlin from './assets/kotlin.svg';
 import Mail from './assets/mail.svg';
+import Motion from './assets/motion.svg';
+import NestJS from './assets/nestjs.svg';
+import NextJS from './assets/nextjs.svg';
+import Pencil from './assets/pencil.svg';
 import Phone from './assets/phone.svg';
+import PostgreSQL from './assets/postgresql.svg';
+import Python from './assets/python.svg';
+import React from './assets/react.svg';
+import Rollup from './assets/rollup.svg';
+import School from './assets/school.svg';
+import SpringBoot from './assets/spring-boot.svg';
+import Storybook from './assets/storybook.svg';
+import SwiftUI from './assets/swiftui.svg';
+import Trophy from './assets/trophy.svg';
+import TypeScript from './assets/typescript.svg';
+import VanillaExtract from './assets/vanilla-extract.svg';
 import Velog from './assets/velog.svg';
 
 export enum IconName {
@@ -14,6 +34,26 @@ export enum IconName {
   PHONE = 'phone',
   INSTAGRAM = 'instagram',
   COPY = 'copy',
+  SCHOOL = 'school',
+  PENCIL = 'pencil',
+  TROPHY = 'trophy',
+  NESTJS = 'nestjs',
+  TYPESCRIPT = 'typescript',
+  POSTGRESQL = 'postgresql',
+  SWIFTUI = 'swiftui',
+  FASTAPI = 'fastapi',
+  MOTION = 'motion',
+  VANILLA_EXTRACT = 'vanilla-extract',
+  NEXTJS = 'nextjs',
+  PYTHON = 'python',
+  STORYBOOK = 'storybook',
+  ROLLUP = 'rollup',
+  REACT = 'react',
+  BUN = 'bun',
+  EMBEDDED = 'embedded',
+  KOTLIN = 'kotlin',
+  SPRING_BOOT = 'spring-boot',
+  DOCKER = 'docker',
 }
 
 export const IconNameMap = {
@@ -24,4 +64,24 @@ export const IconNameMap = {
   [IconName.PHONE]: Phone,
   [IconName.INSTAGRAM]: Instagram,
   [IconName.COPY]: Copy,
+  [IconName.SCHOOL]: School,
+  [IconName.PENCIL]: Pencil,
+  [IconName.TROPHY]: Trophy,
+  [IconName.NESTJS]: NestJS,
+  [IconName.TYPESCRIPT]: TypeScript,
+  [IconName.POSTGRESQL]: PostgreSQL,
+  [IconName.SWIFTUI]: SwiftUI,
+  [IconName.FASTAPI]: FastAPI,
+  [IconName.MOTION]: Motion,
+  [IconName.VANILLA_EXTRACT]: VanillaExtract,
+  [IconName.NEXTJS]: NextJS,
+  [IconName.PYTHON]: Python,
+  [IconName.STORYBOOK]: Storybook,
+  [IconName.ROLLUP]: Rollup,
+  [IconName.REACT]: React,
+  [IconName.BUN]: Bun,
+  [IconName.EMBEDDED]: Embedded,
+  [IconName.KOTLIN]: Kotlin,
+  [IconName.SPRING_BOOT]: SpringBoot,
+  [IconName.DOCKER]: Docker,
 } as const;

f54b6e5bca9d95572859f5725a1d261ad8384a83
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-01-29 03:25:15 +0900

    feat: add @vercel/speed-insights dependency and integrate SpeedInsights component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata, Viewport } from 'next';
 import * as s from './layout.css';
 
-import '@/styles/global.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
+import '@/styles/global.css';
 export const metadata: Metadata = {
   title: 'Jeewon Kwon',
   description: 'Sunrin Internet High School Student',
@@ -23,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang='ko'>
       <body className={s.layout}>{children}</body>
+      <SpeedInsights />
     </html>
   );
 }
